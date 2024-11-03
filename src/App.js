357f05bbd363f1da41ba5cf37e0ba84b39ed1abe
naomitek <156701780+naomitek@users.noreply.github.com>
2024-11-02 18:41:14 -0700

    lab 9

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab Exercise</h2>
+        <p>Your Student ID: 101441399</p> 
+        <p>Your Name: Naomi Teklu</p>
+        <p>George Brown College, Toronto</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
